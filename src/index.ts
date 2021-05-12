a0f339f93f003d18d9e8d797a9817d0100423c2c
Cristian Parra <cristian.a.parra@gmail.com>
2021-05-12 19:23:48 -0400

    list para github

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,24 @@
     ===== Código de TypeScript =====
 */
 
+function reportableClassDecorator<T extends { new (...args: any[]): {} }>(
+    constructor: T
+    ) {
+      return class extends constructor {
+      reportingURL = "http://www...";
+    };
+  }
+  
+  @reportableClassDecorator
+  class BugReport {
+    type = "report";
+    title: string;
+  
+    constructor(t: string) {
+      this.title = t;
+    }
+  }
+  
+  const bug = new BugReport("Needs dark mode");
+  console.log(bug.title); // Prints "Needs dark mode"
+  console.log(bug.type); // Prints "report"
